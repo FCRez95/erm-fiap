fb980c915dbce88a280ec43702bff362065a6f1e
Fabio Rezende <fcrez95@gmail.com>
2023-09-22 00:25:01 -0300

    Delete useless files

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 create table users (
-    id bigint not null,
+    id bigint not null auto_increment,
     name varchar(255) not null,
     email varchar(255) not null,
     password varchar(255) not null,
