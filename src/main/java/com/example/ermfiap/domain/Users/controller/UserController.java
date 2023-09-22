3546839dafdffcec9017b79521f76ca3bb579e99
Fabio Rezende <fcrez95@gmail.com>
2023-09-22 07:52:03 -0300

    feat: add password hasher on user signup

--- src/main/java/com/example/ermfiap/domain/Users/controller/UserController.java
+++ src/main/java/com/example/ermfiap/domain/Users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.ermfiap.domain.Users.controller;
 import com.example.ermfiap.domain.Users.entity.Users;
 import com.example.ermfiap.domain.Users.repository.UsersSprigDataRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,6 +26,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<Users> signUp(@RequestBody Users user) {
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(10)));
         Users createdUser = repository.save(user);
 
         URI newLocation = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.getId()).toUri();
